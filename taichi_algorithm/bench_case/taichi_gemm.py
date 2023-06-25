06a055f55ce05e489c61be38ebfe7c8391539861
garryling <linggaoyuan@gmail.com>
2023-06-25 08:14:37 +0800

    fix: gemm

--- taichi_algorithm/bench_case/taichi_gemm.py
+++ taichi_algorithm/bench_case/taichi_gemm.py
@@ -19,27 +19,24 @@ elif args.type == 'compile':
 ti.init(arch=ti.vulkan)
 if ti.lang.impl.current_cfg().arch != ti.vulkan:
     raise RuntimeError("Vulkan is not available.")
-m = 256
-n = 4096
-k = 4096
 
-A_f = ti.field(ti.f32, shape=(m, k))
-B_f = ti.field(ti.f32, shape=(k, n))
-C_f = ti.field(ti.f32, shape=(m, n))
-
-    
-@ti.func
-def inner(i, j):
-    for ke in range(k):
-        C_f[i, j] += A_f[i, ke] * B_f[ke, j]
-    
 @ti.kernel
-def gemm():
+def gemm(C_f: ti.types.ndarray(dtype=ti.f32, ndim=2), A_f: ti.types.ndarray(dtype=ti.f32, ndim=2), B_f: ti.types.ndarray(ti.f32, ndim=2)):
+
     for i,j in C_f:
-        inner(i, j)
+        for ke in range(k):
+            C_f[i, j] += A_f[i, ke] * B_f[ke, j]
 
 if __name__ == "__main__":
     if run_type == RUN_TYPE.RUN_DIRECT: 
+        m = 256
+        n = 4096
+        k = 4096
+
+        A_f = ti.field(ti.f32, shape=(m, k))
+        B_f = ti.field(ti.f32, shape=(k, n))
+        C_f = ti.field(ti.f32, shape=(m, n))
+
         gemm()
     elif run_type == RUN_TYPE.COMPILE:
         mod = ti.aot.Module(ti.vulkan)
