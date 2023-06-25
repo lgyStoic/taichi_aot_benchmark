d1fc090b0fbe8e215039b855b76b9a5084ae3753
garryling <linggaoyuan@gmail.com>
2023-06-25 08:02:47 +0800

    feat: add gemm and andriod so

--- taichi_algorithm/bench_case/taichi_gemm.py
+++ taichi_algorithm/bench_case/taichi_gemm.py
@@ -19,14 +19,29 @@ elif args.type == 'compile':
 ti.init(arch=ti.vulkan)
 if ti.lang.impl.current_cfg().arch != ti.vulkan:
     raise RuntimeError("Vulkan is not available.")
+m = 256
+n = 4096
+k = 4096
 
+A_f = ti.field(ti.f32, shape=(m, k))
+B_f = ti.field(ti.f32, shape=(k, n))
+C_f = ti.field(ti.f32, shape=(m, n))
+
+    
 @ti.func
+def inner(i, j):
+    for ke in range(k):
+        C_f[i, j] += A_f[i, ke] * B_f[ke, j]
+    
+@ti.kernel
 def gemm():
-    pass
-
+    for i,j in C_f:
+        inner(i, j)
 
 if __name__ == "__main__":
     if run_type == RUN_TYPE.RUN_DIRECT: 
         gemm()
     elif run_type == RUN_TYPE.COMPILE:
-        pass
+        mod = ti.aot.Module(ti.vulkan)
+        mod.add_kernel(gemm)
+        mod.archive("taichi_gemm.tcm")
