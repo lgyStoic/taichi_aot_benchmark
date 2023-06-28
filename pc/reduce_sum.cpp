854e0ec6ace485aff410729f3e867df2289a69c6
garryling <linggaoyuan@gmail.com>
2023-06-28 08:24:08 +0800

    feat: add reduce app content

---
+++ pc/reduce_sum.cpp
@@ -0,0 +1,19 @@
+#include "reduce_sum.h"
+
+namespace taichi_aot {
+
+void REDUCE_APP::prepare() {
+
+if (!hasCreated_) {std::cout << "wrong in create!! "<< std::endl;}
+
+}
+
+void REDUCE_APP::run() {
+
+}
+
+void REDUCE_APP::output() {
+    
+}
+
+}
--- pc/reduce_sum.h
+++ pc/reduce_sum.h
@@ -1,4 +1,19 @@
 #ifndef __REDUCE_SUM_H__
 #define __REDUCE_SUM_H__
+#include "base.h"
 
+namespace taichi_aot {
+
+class REDUCE_APP:public AOT_APP {
+public:
+
+void prepare() override;
+
+void run() override;
+
+void output() override;
+    
+};
+
+}
 #endif
